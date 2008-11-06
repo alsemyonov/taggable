99ac8a75d91a1ed61ef778d91abc46d9f8cb8f92
roTuKa <rotuka@rotuka.com>
2008-11-07 00:57:10 +0300

    Generate models now

--- generators/acts_as_taggable_migration/acts_as_taggable_migration_generator.rb
+++ generators/acts_as_taggable_migration/acts_as_taggable_migration_generator.rb
@@ -1,7 +1,10 @@
 class ActsAsTaggableMigrationGenerator < Rails::Generator::Base 
   def manifest 
     record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate' 
+      m.migration_template 'migration.rb', 'db/migrate'
+      m.file "tag.rb", "app/models/tag.rb"
+      m.file "taging.rb", "app/models/tagging.rb"
+      m.file "tags_helper.rb", "app/helpers/tags_helper.rb"
     end 
   end
   
